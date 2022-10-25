2bf133249f48e72a01767ac1d8b34a279750998e
Jan Wloka <jan.wloka@quatico.com>
2022-10-25 15:21:13 +0200

    t Adds tests to indicate the problem with methods
    issue #17147

--- code/renderers/web-components/src/docs/custom-elements.test.ts
+++ code/renderers/web-components/src/docs/custom-elements.test.ts
@@ -2,7 +2,7 @@
 // @ts-expect-error (Converted from ts-ignore)
 import global from 'global';
 
-import { extractArgTypes } from './custom-elements';
+import { extractArgTypes, extractComponentDescription } from './custom-elements';
 import customElementsManifest from './__testfixtures__/custom-elements.json';
 
 declare global {
@@ -46,6 +46,41 @@ describe('extractArgTypes', () => {
           type: { summary: 'CustomEvent' },
           defaultValue: { summary: undefined },
         },
+      });
+    });
+  });
+
+  describe('members', () => {
+    it('should map to a method item with distinct name', () => {
+      const { doClose: item } = extractArgTypes('sb-dialog-working');
+
+      expect(item).toEqual({
+        description: 'Close the dialog',
+        name: 'doClose',
+        required: false,
+        table: {
+          category: 'properties',
+          defaultValue: { summary: undefined },
+          type: { summary: undefined },
+        },
+        type: { name: undefined },
+      });
+    });
+
+    // FIXME: The current implementation of mapData cannot deal clashing names, e.g., member vs. event
+    it.skip('should map to a method item with clashing event name', () => {
+      const { 'event#close': item } = extractArgTypes('sb-dialog-broken');
+
+      expect(item).toEqual({
+        description: 'Close the dialog',
+        name: 'close',
+        required: false,
+        table: {
+          category: 'properties',
+          defaultValue: { summary: undefined },
+          type: { summary: undefined },
+        },
+        type: { name: undefined },
       });
     });
   });
